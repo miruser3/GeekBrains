53ade0499f844408bc65147b973fe12fde9c7c30
mir <mika___i87@mail.ru>
2022-05-06 02:21:06 +0500

    creat_directory

--- raectjs/myapp/src/App.js
+++ raectjs/myapp/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Creatmessage from './Message';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className={`App-header ${props.showred ? 'header-red' : 'header-blue'}`}
+      style={{ top: props.topPosition || '10px' }}>
+        My First React App
+        <h3>Hello, {props.name}</h3>
+        
       </header>
+      < Creatmessage />
     </div>
   );
 }
---
+++ raectjs/myapp/src/Message.js
@@ -0,0 +1,7 @@
+import './App.css';
+
+function Creatmessage() {
+    return <div className='massage'> Creatmessage Text </div>
+}
+
+export default Creatmessage;
